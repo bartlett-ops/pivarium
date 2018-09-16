2b8edbb2dfc905932966099c2cb59c12876d892c
Tom Bartlett <thomas.bartlett.1992@gmail.com>
2018-09-16 11:45:16 +0000

    bugfix

--- src/water_level.py
+++ src/water_level.py
@@ -27,12 +27,12 @@ def init():
   GPIO.output(RED, False)
   GPIO.output(AMBER, False)
   GPIO.output(GREEN, False)
-  
+
 
 def get_distance():
   GPIO.output(TRIG, False)
   time.sleep(1)
-  
+
   GPIO.output(TRIG, True)
   time.sleep(0.00001)
   GPIO.output(TRIG, False)
@@ -44,7 +44,7 @@ def get_distance():
   pulse_end = time.time()
   while GPIO.input(ECHO)==1:
     pulse_end = time.time()
-  
+
   pulse_duration = pulse_end - pulse_start
   
   distance = pulse_duration * 17150
